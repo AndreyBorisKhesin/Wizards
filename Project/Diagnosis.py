5dfd2470c86566c93b7cba5ba7a084fcc3741d6e
christinekc <Chrsitine@Christines-MacBook-Pro.local>
2016-11-26 18:58:50 -0500

    appeasing one of my teammates

--- Project/Diagnosis.py
+++ Project/Diagnosis.py
@@ -1,5 +1,10 @@
 class Diagnosis:
 
-    def __init__(self):
-        self.disease
-        self.probability
+    def __init__(self, disease = "", probability = 0.0):
+
+        ''' A Person's diagnosis
+
+        :return: None
+        '''
+        self.disease = disease
+        self.probability = probability
