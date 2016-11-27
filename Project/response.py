021704cb10a1658e1b9747fc87726b452b9630b1
Neumann Hon <neumannkachun.hon@mail.utoronto.ca>
2016-11-26 23:23:22 -0500

    attempting to resolve conflict

--- Project/response.py
+++ Project/response.py
@@ -1,16 +1,20 @@
-from Person import Person
 from message import Message
+from Person import*
+class Response:
+    people = []
 
-def generateresponse(person, text):
-    """
-    Generate a response given a phone number and message
+    def generateresponse(phonenumber, text, time):
+        """
+        Generate a response given a phone number and message
 
-    :param phonenumber:
-    :type phonenumber:
-    :param message:
-    :type message:
-    :return:
-    :rtype:
-    """
+        :param phonenumber: int
+                The person's phone number
+        :param text: str
+                The person's message
+        :rtype: str
+        """
+        found = False
+        for peeps in Response.people:
+            if peeps
 
 
